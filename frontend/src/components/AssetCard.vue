7d52e3b553c6f67fc75829f2663a3e5d09a64db8
maladontok <18andreano2006@gmail.com>
2026-02-05 17:53:52 +0300

    Create AssetCard.vue

---
+++ frontend/src/components/AssetCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-card-title>{{ asset.name }}</v-card-title>
+    <v-card-subtitle>Инв. № {{ asset.inventory_number }}</v-card-subtitle>
+    <v-card-text>
+      <div>Стоимость: {{ formatCurrency(asset.current_cost) }}</div>
+      <div>Статус: {{ asset.status }}</div>
+      <div>Ответственный: {{ asset.responsible?.name || 'Не назначен' }}</div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['asset'],
+  methods: {
+    formatCurrency(value) {
+      return new Intl.NumberFormat('ru-RU', {
+        style: 'currency',
+        currency: 'RUB'
+      }).format(value);
+    }
+  }
+}
+</script>
